cc2d82667ee97a3cfb849c9015a0774a550ecf77
Yihui Xie <xie@yihui.name>
2020-09-24 23:02:12 -0500

    start the project: compile.R tries to compile documents, and latex-pass.R tells you which LaTeX packages will be required to compile these documents

---
+++ README.md
@@ -0,0 +1,17 @@
+# LaTeX Pass
+
+Upload a LaTeX error log file, or a LaTeX document, or an R Markdown document to this repository, and I will tell you which LaTeX packages you need to install in your local LaTeX distribution so you can compile your documents to PDF. You will no longer be confused by LaTeX error messages like this:
+
+```latex
+! LaTeX Error: File `inconsolata.sty' not found.
+
+Type X to quit or <RETURN> to proceed,
+or enter new name. (Default extension: sty)
+
+! Emergency stop.
+<read *>
+
+l.276 ^^M
+
+!  ==> Fatal error occurred, no output PDF file produced!
+```
---
+++ compile.R
@@ -0,0 +1,34 @@
+# install TinyTeX
+install.packages('xfun')
+xfun::pkg_load2('tinytex')
+tinytex::install_tinytex()
+
+p0 = tinytex::tl_pkgs()  # the initial set of LaTeX packages installed
+p1 = NULL  # missing packages identified from the LaTeX log
+
+# parse LaTeX errors from .log files, or compile .Rmd/.tex files to figure out
+# missing LaTeX packages
+for (f in list.files('.', '[.](Rmd|tex|log)$')) {
+  # different actions according to filenames extensions
+  switch(
+    xfun::file_ext(f),
+    Rmd = if (length(grep('^---\\s*$', readLines(f, n = 1)))) {
+      # the first line needs to be ---, in case it's a child Rmd file
+      xfun::pkg_load2('rmarkdown')
+      rmarkdown::render(f)
+    },
+    tex = if (length(grep('\\\\(documentclass|begin\\{document\\})', readLines(f))) >= 2) {
+      # need to find \documentclass or \begin{document} in the .tex file
+      r = '.*?((?:pdf|xe|lua)?latex).*'
+      engine = if (length(grep(r, f))) gsub(r, '\\1', f) else 'pdflatex'
+      r = '.*?(bibtex|biber).*'
+      bib_engine = if (length(grep(r, f))) gsub(r, '\\1', f) else 'bibtex'
+      tinytex::latexmk(f, engine = engine, bib_engine = bib_engine)
+    },
+    log = {
+      p1 = c(p1, tinytex::parse_packages(f))
+    }
+  )
+}
+p2 = setdiff(tinytex::tl_pkgs(), p0)  # newly installed packages after compiling Rmd/tex files
+saveRDS(list(p1, p2), file = 'packages.rds')
