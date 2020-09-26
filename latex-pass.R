d20dddd9eae482eb5c4273018e53ecc4fb49dff7
Yihui Xie <xie@yihui.name>
2020-09-26 16:49:19 -0500

    the env var APPVEYOR_PULL_REQUEST_NUMBER is "" when the build is not from a PR, so need to test == "" instead of "unset"

--- latex-pass.R
+++ latex-pass.R
@@ -4,13 +4,13 @@ pp = readRDS('packages.rds')
 p1 = pp[[1]]; p2 = pp[[2]]
 
 msg = c(
-  if (length(p1)) c('The missing packages identified from the LaTeX log are:\n\n ', p1),
+  if (length(p1)) c('The missing packages identified from the LaTeX log are:\n\n    ', p1),
   if (length(p2)) c('\n\nAdditional packages required to compile your documents are:\n\n    ', p2)
 )
 msg = if (length(msg)) {
   p = sort(unique(c(p1, p2)))
-  message(paste(msg, collapse = ' '))
   c(
+    msg,
     sprintf(
       '\n\nIf you are an R user using TinyTeX, you may install these packages via:\n\n    tinytex::tlmgr_install(c(%s))',
       paste0('"', p, '"', collapse = ', ')
@@ -25,7 +25,7 @@ msg = if (length(msg)) {
 message(msg)
 
 # add a comment on the PR
-if (length(msg) && !is.na(p <- Sys.getenv('APPVEYOR_PULL_REQUEST_NUMBER', NA))) system2('curl', c(
+if (length(msg) && Sys.getenv('APPVEYOR_PULL_REQUEST_NUMBER') != '') system2('curl', c(
   '-u', 'yihui:${GH_TOKEN}', '-X', 'POST', '-H', '"Accept: application/vnd.github.v3+json"',
   'https://api.github.com/repos/${APPVEYOR_REPO_NAME}/issues/${APPVEYOR_PULL_REQUEST_NUMBER}/comments',
   '-d', shQuote(xfun::tojson(list(body = paste(msg, collapse = ''))))
