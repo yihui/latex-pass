83437e24b59693f38343e8e81ef418c1418c6c6a
Yihui Xie <xie@yihui.name>
2020-09-26 16:33:21 -0500

    just post a comment to the PR via Github API, so users don't need to read the AppVeyor log

--- latex-pass.R
+++ latex-pass.R
@@ -3,20 +3,28 @@ p1 = pp[[1]]; p2 = pp[[2]]
 
 msg = c(
   if (length(p1)) c('The missing packages identified from the LaTeX log are:\n\n ', p1),
-  if (length(p2)) c('\n\nAdditional packages required to compile your documents are:\n\n ', p2)
+  if (length(p2)) c('\n\nAdditional packages required to compile your documents are:\n\n    ', p2)
 )
-if (length(msg)) {
+msg = if (length(msg)) {
   p = sort(unique(c(p1, p2)))
   message(paste(msg, collapse = ' '))
-  message(
+  c(
     sprintf(
-      '\nIf you are an R user using TinyTeX, you may install these packages via:\n\n  tinytex::tlmgr_install(c(%s))',
+      '\n\nIf you are an R user using TinyTeX, you may install these packages via:\n\n    tinytex::tlmgr_install(c(%s))',
       paste0('"', p, '"', collapse = ', ')
     ),
-    '\n\nIf you use TinyTeX but are not an R user, you may install these packages via command line:\n\n  tlmgr install ',
+    '\n\nIf you use TinyTeX but are not an R user, you may install these packages via command line:\n\n    tlmgr install ',
     paste(p, collapse = ' '),
     '\n\nIf you do not use TinyTeX (https://yihui.org/tinytex/), you need to figure out how to install them by yourself.'
   )
 } else {
-  message('I did not figure out which LaTeX packages you need to install. Sorry.')
+  'I did not figure out which LaTeX packages you need to install. Sorry.'
 }
+message(msg)
+
+# add a comment on the PR
+if (length(msg) && !is.na(p <- Sys.getenv('APPVEYOR_PULL_REQUEST_NUMBER', NA))) system2('curl', c(
+  '-u', 'yihui:${GH_TOKEN}', '-X', 'POST', '-H', '"Accept: application/vnd.github.v3+json"',
+  'https://api.github.com/repos/${APPVEYOR_REPO_NAME}/issues/${APPVEYOR_PULL_REQUEST_NUMBER}/comments',
+  '-d', shQuote(xfun::tojson(list(body = paste(msg, collapse = ''))))
+))
