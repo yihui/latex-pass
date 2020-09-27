495818c4a2b83b1d481cec7e92dbbdc2a24ef00d
Yihui Xie <xie@yihui.name>
2020-09-26 23:06:08 -0500

    separate package names by spaces

--- latex-pass.R
+++ latex-pass.R
@@ -2,10 +2,11 @@
 
 pp = readRDS('packages.rds')
 p1 = pp[[1]]; p2 = pp[[2]]
+one_string = function(..., s = ' ') paste(..., collapse = s)
 
 msg = c(
-  if (length(p1)) c('The missing packages identified from the LaTeX log are:\n\n    ', p1),
-  if (length(p2)) c('\n\nAdditional packages required to compile your documents are:\n\n    ', p2)
+  if (length(p1)) c('The missing packages identified from the LaTeX log are:\n\n    ', one_string(p1)),
+  if (length(p2)) c('\n\nAdditional packages required to compile your documents are:\n\n    ', one_string(p2))
 )
 msg = if (length(msg)) {
   p = sort(unique(c(p1, p2)))
@@ -13,14 +14,14 @@ msg = if (length(msg)) {
     msg,
     sprintf(
       '\n\nIf you are an R user using TinyTeX, you may install these packages via:\n\n    tinytex::tlmgr_install(c(%s))',
-      paste0("'", p, "'", collapse = ', ')
+      one_string("'", p, "'", s = ', ')
     ),
     '\n\nIf you use TinyTeX but are not an R user, you may install these packages via command line:\n\n    tlmgr install ',
-    paste(p, collapse = ' '),
+    one_string(p),
     '\n\nIf you do not use TinyTeX (https://yihui.org/tinytex/), you need to figure out how to install them by yourself.'
   )
 } else {
   'I did not figure out which LaTeX packages you need to install. Sorry.'
 }
 message(msg)
-writeLines(paste(msg, collapse = ''), 'message.txt')
+writeLines(one_string(msg, s = ''), 'message.txt')
