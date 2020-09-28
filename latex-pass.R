ad2c32b7ab746705e7c47616628eb14fd6a4c51e
Yihui Xie <xie@yihui.name>
2020-09-28 09:21:27 -0500

    try to put more information in Details because the message has a 500-character limit: https://help.appveyor.com/discussions/problems/28277

--- latex-pass.R
+++ latex-pass.R
@@ -8,10 +8,9 @@ msg = c(
   if (length(p1)) c('The missing packages identified from the LaTeX log are:\n\n    ', one_string(p1)),
   if (length(p2)) c('\n\nAdditional packages required to compile your documents are:\n\n    ', one_string(p2))
 )
-msg = if (length(msg)) {
+msg2 = if (length(msg)) {
   p = sort(unique(c(p1, p2)))
   c(
-    msg,
     sprintf(
       '\n\nIf you are an R user using TinyTeX, you may install these packages via:\n\n    tinytex::tlmgr_install(c(%s))',
       one_string(sprintf("'%s'", p), s = ', ')
@@ -21,7 +20,9 @@ msg = if (length(msg)) {
     '\n\nIf you do not use TinyTeX (https://yihui.org/tinytex/), you need to figure out how to install them by yourself.'
   )
 } else {
-  'I did not figure out which LaTeX packages you need to install. Sorry.'
+  msg = 'I did not figure out which LaTeX packages you need to install. Sorry.'
+  ""
 }
-message(msg)
+message(msg, msg2)
 writeLines(one_string(msg, s = ''), 'message.txt')
+writeLines(one_string(msg2, s = ''), 'details.txt')
